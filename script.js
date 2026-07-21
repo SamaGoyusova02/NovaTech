89f47b6c6f2c34364d276f3ad7aa4004c8206f6f
Sama <semagousova009@gmail.com>
2026-07-21 19:52:58 +0400

    checkPoint4

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Menubar
 function OpenMenu(){
 const MobilMenubar = document.getElementById('MobilMenubar')
 const mark = document.getElementById('mark')
@@ -12,3 +13,27 @@ const mark = document.getElementById('mark')
         mark.classList.replace('fa-xmark', 'fa-bars-staggered');
     }
 }
+// Regexp
+document.getElementById('contactForm').addEventListener('submit', function (e) {
+    e.preventDefault(); 
+
+    const fullname = document.getElementById('fullname').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+    const nameRegex = /^[A-Za-zƏəĞğIıİiÖöŞşÜüÇç]{2,}\s+[A-Za-zƏəĞğIıİiÖöŞşÜüÇç]{2,}/;
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (!nameRegex.test(fullname)) {
+        alert("Zəhmət olmasa Ad və Soyadınızı düzgün daxil edin! (Nümunə: Əli Məmmədov)");
+        return;
+    }
+    if (!emailRegex.test(email)) {
+        alert("Zəhmət olmasa düzgün elektron poçt ünvanı daxil edin!");
+        return;
+    }
+    if (message.length < 10) {
+        alert("Mesajınız ən azı 10 simvoldan ibarət olmalıdır!");
+        return;
+    }
+    alert("Mesajınız uğurla göndərildi!");
+    this.reset(); 
+});
